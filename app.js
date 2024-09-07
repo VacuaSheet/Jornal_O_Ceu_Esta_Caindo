46c3ad5d58d39e3b507a8858311c62c5e00e9176
Tiago Santos <97613322+VacuaSheet@users.noreply.github.com>
2024-09-06 21:11:00 -0300

    v0.1

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    
+    if (campoPesquisa == "") {
+      section.innerHTML = "<p>Nada foi encontrado pois nada foi digitado</p>"
+      return
+    };
+    // Função para remover acentos
+    function removerAcentos(str) {
+      return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    }
+    campoPesquisa = removerAcentos(campoPesquisa);
+    console.log(campoPesquisa)
+     
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+  
+    // Itera sobre cada dado da pesquisa
+    for (let dado of dados) { 
+      titulo = removerAcentos(dado.titulo);
+      descricao = removerAcentos(dado.descricao);
+      let tags = dado.tags.map(tag => removerAcentos(tag));
+      // Constrói o HTML para cada resultado da pesquisa, formatando os dados
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais informações</a>
+        </div>
+      `;
+      }
+    }
+      if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+      };
+      
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Guerra na Ucrânia",
+        descricao : "A guerra na Ucrânia, iniciada em 24 de fevereiro de 2022 com a invasão russa, representa um dos maiores conflitos armados na Europa desde a Segunda Guerra Mundial. O conflito, que teve início com a anexação da Crimeia pela Rússia em 2014, escalou para uma invasão em larga escala, causando uma crise humanitária de proporções gigantescas. Milhares de civis perderam a vida, milhões foram deslocados internamente ou buscaram refúgio em outros países, e infraestruturas essenciais foram destruídas. As consequências da guerra se estendem além das fronteiras ucranianas, impactando a economia global, a segurança energética e a ordem internacional. A comunidade internacional impôs sanções severas à Rússia em resposta à agressão, mas os esforços para uma solução pacífica ainda enfrentam desafios significativos.",
+        link : "https://www.aljazeera.com/tag/ukraine-russia-crisis/",
+        tags: ["Ucrânia", "Rússia", "Guerra", "Conflito", "05/09/2024"]
+    },
+    {
+        titulo: "Polônia envia aviões enquanto Rússia ataca o oeste da Ucrânia",
+        descricao: "Descrição detalhada sobre o envio de aviões pela Polônia, os alvos dos ataques russos no oeste da Ucrânia e as implicações para o conflito. Inclua informações sobre o tipo de aviões, a intensidade dos ataques, a reação da comunidade internacional, etc.",
+        link: "https://www.aljazeera.com/news/2024/9/4/poland-scrambles-planes-as-russia-strikes-western-ukraine",
+        tags: ["Polônia", "Aviões", "Rússia", "Ucrânia", "01/09/2024"]
+    },
+    {
+        titulo: "As perdas da Ucrânia superam os ganhos de Kursk, com a Rússia prestes a tomar Pokrovsk",
+        descricao: "Descrição detalhada sobre as perdas da Ucrânia, a importância estratégica de Kursk e a ameaça à cidade de Pokrovsk. Inclua dados como número de baixas, avanço das tropas russas, impacto na população civil, etc.",
+        link: "https://www.aljazeera.com/features/2024/9/4/ukraines-losses-outweigh-kursk-gains-as-russia-on-cusp-of-taking-key-town",
+        tags:  ["Ucrânia", "Kursk", "Rússia", "Pokrovsk", "03/09/2024"]
+    }
+];
